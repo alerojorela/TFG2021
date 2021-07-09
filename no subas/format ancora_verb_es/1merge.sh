d572950c95cbbfc876dcee673f747417a7ee3c7f
alerojorela <alejandro.rojo@protonmail.com>
2021-07-09 14:11:44 +0200

    adding files

--- no subas/format ancora_verb_es/1merge.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# echo <?xml version="1.0" encoding="UTF-8"?> > merged.xml
-echo > merged.xml
-for filename in ancora-verb-es/*.xml; do
-    xmlstarlet sel -B -t -c 'lexentry' "$filename" >> merged.xml
-    echo >> merged.xml
-done
-
-# xmlstarlet sel -B -t -c 'lexentry' aullar.lex.xml
